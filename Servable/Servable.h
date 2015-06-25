b6e873ed713b0b788cbc0b8433fc1f28120c1ac2
Florent Vilmart <florentvilmart@me.com>
2015-06-25 13:13:02 -0400

    Initial Commit

---
+++ Servable/Servable.h
@@ -0,0 +1,19 @@
+//
+//  Servable.h
+//  Servable
+//
+//  Created by Florent Vilmart on 2015-06-25.
+//  Copyright © 2015 flovilmart. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Servable.
+FOUNDATION_EXPORT double ServableVersionNumber;
+
+//! Project version string for Servable.
+FOUNDATION_EXPORT const unsigned char ServableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Servable/PublicHeader.h>
+
+
